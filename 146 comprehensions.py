63658b79344e108b630bf3e1ffbaeb89276dc983
Kons <katashi.rain@gmail.com>
2021-09-06 20:57:39 +0900

    146 comprehensions done

--- 146 comprehensions.py
+++ 146 comprehensions.py
@@ -15,3 +15,14 @@ print(new_list)
 print(new_list2)
 print(new_list3)
 print(new_list4)
+
+# dict comprehension
+simple_dict = {
+    'a': 1,
+    'b': 2
+}
+my_dict = {key: value**2 for key, value in simple_dict.items() if value%2 == 0}
+print(my_dict)
+
+new_dict = {num: num*2 for num in [1,2,3]}
+print(new_dict)
