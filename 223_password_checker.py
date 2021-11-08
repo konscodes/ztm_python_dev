ea91c3439478873dd1905485ef5fe8f68fdfdb41
Kons <katashi.rain@gmail.com>
2021-11-08 21:55:22 +0900

    223 adding functions

--- 223_password_checker.py
+++ 223_password_checker.py
@@ -3,21 +3,28 @@ import requests
 import hashlib
 
 hash_list = []
-for password in argv[1:]:
+
+def get_hash(password):
+    global hash_list
     password = password.encode('utf-8')
     print(type(password))
     hash = hashlib.sha1(password).hexdigest()
     hash_list.append(hash.upper())
 
-print(hash_list)
 
-for given_hash in hash_list:
-    query = requests.get('https://api.pwnedpasswords.com/range/' + given_hash[:5])
+def query(hash):
+    query = requests.get('https://api.pwnedpasswords.com/range/' + hash[:5])
     print(query.status_code)
     data_list = query.text.split()
     data_dict = {}
     for item in data_list:
         key, value = item.split(sep=':')
         data_dict[key] = value
-
     print(data_dict)
+
+
+for password in argv[1:]:
+    get_hash(password)
+
+for hash in hash_list:
+    query(hash)
