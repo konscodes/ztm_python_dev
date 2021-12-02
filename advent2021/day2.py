ce5c4646dd951fef6c3ea1ccf05f364d8614a83e
konscodes <katashi.rain@gmail.com>
2021-12-03 00:54:47 +0900

    day2 part2 completed without pandas

--- advent2021/day2.py
+++ advent2021/day2.py
@@ -11,28 +11,42 @@ from pathlib import Path
 
 script_path = Path(__file__).resolve()
 script_parent = script_path.parent
-data_file_path = script_parent / 'files' / 'test2.txt'
+data_file_path = script_parent / 'files' / 'day2.txt'
 
 def read_data(file_path):
     with open(file_path) as file_object:
         data = file_object.read()
         data = data.split(sep='\n')
         data_list = [i.split(sep=' ') for i in data]
-        data_dict = {'forward':0, 'down':0, 'up':0, 'depth':0, 'position':0, 'aim':0, 'new_position':0}
-        for item in data_list:
-            key, value = item[0], int(item[1])
-            data_dict[key] = data_dict.get(key) + value
-            if key == 'down':
-                data_dict['aim'] = data_dict.get('aim') + value
-                data_dict['depth'] = data_dict.get('depth') + value
-            elif key == 'up':
-                data_dict['aim'] = data_dict.get('aim') - value
-                data_dict['depth'] = data_dict.get('depth') - value
-            else:
-                data_dict['depth'] = data_dict.get('aim') * value
-        data_dict['position'] = data_dict['forward'] * data_dict['depth']
+    return data_list
+
+
+def part1(data_list):
+    data_dict = {'forward':0, 'down':0, 'up':0, 'depth':0, 'position':0}
+    for item in data_list:
+        key, value = item[0], int(item[1])
+        data_dict[key] = data_dict.get(key) + value
+    data_dict['depth'] = data_dict['down'] - data_dict['up']
+    data_dict['position'] = data_dict['forward'] * data_dict['depth']
     return data_dict
 
-result = read_data(data_file_path)
-print(f'Part1 coordinates: {result["position"]}')
-print(f'Part2 coordinates: {result["new_position"]}')
+
+def part2(data_list):
+    data_dict = {'forward':0, 'down':0, 'up':0, 'depth':0, 'position':0, 'aim':0}
+    for item in data_list:
+        key, value = item[0], int(item[1])
+        data_dict[key] = data_dict.get(key) + value
+        if key == 'down':
+            data_dict['aim'] = data_dict.get('aim') + value
+        elif key == 'up':
+            data_dict['aim'] = data_dict.get('aim') - value
+        else:
+            data_dict['depth'] = data_dict.get('depth') + (data_dict.get('aim') * value)
+    data_dict['position'] = data_dict['forward'] * data_dict['depth']
+    return data_dict
+
+data = read_data(data_file_path)
+result1 = part1(data)
+result2 = part2(data)
+print(f'Part1 coordinates: {result1["position"]}')
+print(f'Part2 coordinates: {result2["position"]}')
