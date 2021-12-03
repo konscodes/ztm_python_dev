8e2c6748920272b7681a1dfb0feefe1ab46eeac6
Kons <katashi.rain@gmail.com>
2021-12-03 08:11:13 +0000

    day3 part1 solved

--- advent2021/day3.py
+++ advent2021/day3.py
@@ -5,3 +5,55 @@
 # The epsilon rate is 01001, or 9 in decimal. 
 # Multiplying the gamma rate (22) by the epsilon rate (9) produces the power consumption, 198.
 
+from pathlib import Path
+
+script_path = Path(__file__).resolve()
+script_parent = script_path.parent
+data_file_path = script_parent / 'files' / 'test3.txt'
+
+def read_data(file_path):
+    with open(file_path) as file_object:
+        data = file_object.read()
+        data = data.split(sep='\n')
+    return data
+
+
+def get_transposed_data(data: list):
+    keydict = {}
+    for i in range(len(data[0])):
+        keydict[i] = []
+    for line in data:
+        for index, value in enumerate(line):
+            keydict[index].append(value)
+    return keydict
+
+
+def find_common(data: dict):
+    most_common = []
+    least_common = []
+    for values_list in data.values():
+        if values_list.count('0') > values_list.count('1'):
+            most_common.append(0)
+            least_common.append(1)
+        else:
+            most_common.append(1)
+            least_common.append(0)
+    return most_common, least_common
+
+
+def convert_to_decimal(binary: list):
+    decimal = 0
+    for i in range(len(binary)):
+        digit = binary.pop()
+        if digit == 1:
+            decimal = decimal + pow(2, i)
+    return decimal
+
+
+data_list = read_data(data_file_path)
+data_transposed = get_transposed_data(data_list)
+common_value = find_common(data_transposed)
+gamma = convert_to_decimal(common_value[0])
+elipson = convert_to_decimal(common_value[1])
+print(f'Gamma rate is {gamma} and elipson rate is {elipson}')
+print(f'Power consumption is {gamma * elipson}')
