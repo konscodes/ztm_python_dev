bc2c73a0eb462933519a7469c0611fb8dd815593
konscodes <katashi.rain@gmail.com>
2021-12-03 15:24:38 +0900

    adding day3 challenge

---
+++ advent2021/day3.py
@@ -0,0 +1,7 @@
+# Part1
+# Find the most common and the least common bit sequence.
+# Multiply least common decimal to most common decimal.
+# In example data, gamma rate is the binary number 10110, or 22 in decimal.
+# The epsilon rate is 01001, or 9 in decimal. 
+# Multiplying the gamma rate (22) by the epsilon rate (9) produces the power consumption, 198.
+
