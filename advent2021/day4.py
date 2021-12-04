5249a11e2551596367de84d339220b347310bf5b
konscodes <katashi.rain@gmail.com>
2021-12-04 20:34:43 +0900

    read and prep files

--- advent2021/day4.py
+++ advent2021/day4.py
@@ -11,7 +11,18 @@ file_path = absolute_path / 'files' / 'test4.txt'
 def read_data(file_path):
     with open(file_path) as file_object:
         data = file_object.read()
-        print(data)
+        data = data.split(sep='\n\n')
+        numbers = [int(i) for i in data[0].split(sep=',')]
+        # Take list of boards as a str
+        # Separate rows into separate values by \n
+        # Turn str values into int lists
+        boards = {}
+        boards_list = data[1:]
+        for index, board in enumerate(boards_list):
+            print(board)
+            board_row = [i.replace(' ', '  ').split() for i in board.split(sep='\n')]
+            boards[index] = board_row
+        return numbers, boards
 
 
-read_data(file_path)
+print(read_data(file_path))
