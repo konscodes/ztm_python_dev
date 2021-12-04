ba686e8908e1f7031e7d51955d28375c2c6e69d9
konscodes <katashi.rain@gmail.com>
2021-12-04 23:52:41 +0900

    main bingo logic drafted

--- advent2021/day4.py
+++ advent2021/day4.py
@@ -12,17 +12,45 @@ def read_data(file_path):
     with open(file_path) as file_object:
         data = file_object.read()
         data = data.split(sep='\n\n')
-        numbers = [int(i) for i in data[0].split(sep=',')]
+        numbers = data[0].split(sep=',')
         # Take list of boards as a str
         # Separate rows into separate values by \n
         # Turn str values into int lists
         boards = {}
         boards_list = data[1:]
         for index, board in enumerate(boards_list):
-            print(board)
+            #print(board)
             board_row = [i.replace(' ', '  ').split() for i in board.split(sep='\n')]
             boards[index] = board_row
         return numbers, boards
 
 
-print(read_data(file_path))
+def bingo(numbers, boards):
+    winner_board = {'index': 0, 'board': 0}
+    print(numbers)
+    matched = {}
+    for number in numbers:
+        for dict_key in boards.keys():
+            board = boards.get(dict_key)
+            for row_index, row in enumerate(board):
+                print(row)
+                if number in row:
+                # Check if number in each row
+                #   Create a dict of dicts to store the match data
+                #   Save the match row_index, element_index, dict_key
+                #   Increment number of match for row/element index
+                #   If counter == 5
+                #       set winner flag to True
+                #       winner_board = dict_key 
+                #       
+                    pass
+        # Check if winner exists
+        # If winner is True return winner board
+    return winner_board
+
+
+
+numbers = read_data(file_path)[0]
+boards = read_data(file_path)[1]
+
+print(bingo(numbers, boards))
