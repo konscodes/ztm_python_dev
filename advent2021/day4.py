66ed803f390be157166046a486b3ba71c6534d37
konscodes <katashi.rain@gmail.com>
2021-12-05 21:56:11 +0900

    day4 part1 completed

--- advent2021/day4.py
+++ advent2021/day4.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 script_parent = Path(__file__).parent
 absolute_path = script_parent.resolve()
-file_path = absolute_path / 'files' / 'test4.txt'
+file_path = absolute_path / 'files' / 'day4.txt'
 
 def read_data(file_path):
     with open(file_path) as file_object:
@@ -19,38 +19,50 @@ def read_data(file_path):
         boards = {}
         boards_list = data[1:]
         for index, board in enumerate(boards_list):
-            #print(board)
             board_row = [i.replace(' ', '  ').split() for i in board.split(sep='\n')]
             boards[index] = board_row
         return numbers, boards
 
 
 def bingo(numbers, boards):
-    winner_board = {'index': 0, 'board': 0}
     matched = {b:{i:[] for i in range(5)} for b in boards.keys()}
+    matched_columns = {b:{i:[] for i in range(5)} for b in boards.keys()}
     for number in numbers:
         print(number)
         for dict_key in boards.keys():
             board = boards.get(dict_key)
-            for row_index, row in enumerate(board):
-                if number in row:
+            for row_index, row_value in enumerate(board):
+                if number in row_value:
                     matched_dict = matched[dict_key]
-                    matched_dict.get(row_index).append(row.index(number))
+                    matched_dict.get(row_index).append(row_value.index(number))
+                    matched_columns[dict_key][row_value.index(number)].append(1)
+                    if len(matched_dict.get(row_index)) == 5:
+                        print(f'Winning row in board {dict_key} row {row_index}: {matched_dict.get(row_index)}')
+                        return number, matched[dict_key], boards.get(dict_key), 'row', row_index
+                    if len(matched_columns[dict_key][row_value.index(number)]) == 5:
+                        print(f'Winning column in board {dict_key} column {row_value.index(number)}: {matched_dict}')
+                        return number, matched[dict_key], boards.get(dict_key), 'column', row_value.index(number)
                 # Check if number in each row
                 #   Create a dict of dicts to store the match data
                 #   Save the match row_index, element_index, dict_key
                 #   Increment number of match for row/element index
                 #   If counter == 5
-                #       set winner flag to True
-                #       winner_board = dict_key 
-        print(matched)
-        # Check if winner exists
-        # If winner is True return winner board
-    return winner_board
+                #       return winner board
+    return print('No winners detected')
 
 
+def board_score(win_number, matched, win_board):
+    board = win_board.copy()
+    for row_index in matched.keys():
+        value = matched.get(row_index)
+        for i in value:
+            board[row_index][i] = 0
+    board_sum = sum([sum([int(i) for i in row]) for row in board])
+    score = board_sum * int(win_number)
+    return score
 
-numbers = read_data(file_path)[0]
-boards = read_data(file_path)[1]
 
-print(bingo(numbers, boards))
+numbers, boards = read_data(file_path)
+win_number, matched, win_board, row_column, index = bingo(numbers, boards)
+score = board_score(win_number, matched, win_board)
+print(f'Board score is {score}')
