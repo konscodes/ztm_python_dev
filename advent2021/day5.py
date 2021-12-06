c67dfc84653608eb0eab4ca0d6882341c61d7c59
konscodes <katashi.rain@gmail.com>
2021-12-06 23:10:54 +0900

    day5 part2 completed

--- advent2021/day5.py
+++ advent2021/day5.py
@@ -1,10 +1,10 @@
-# Part1
+# Part1 and Part2 in one function
 
 from pathlib import Path
 
 path = Path(__file__).resolve()
 parent = path.parent
-file_path = parent / 'files' / 'test5.txt'
+file_path = parent / 'files' / 'day5.txt'
 
 def read_data(file_path):
     with open(file_path) as file_object:
@@ -15,8 +15,8 @@ def read_data(file_path):
 
 def build_matrix(vents):
     vents = vents.copy()
-    size = max([max(i) for i in vents])
-    matrix = [[0] * size] * size
+    size = max([max(i) for i in vents]) + 1
+    matrix = [[0 for x in range(size)] for i in range(size)]
     return matrix
 
 
@@ -24,14 +24,31 @@ def mark_matrix(matrix, vents):
     new_matrix = matrix.copy()
     for line in vents:
         x1,y1,x2,y2 = line
+        rows = sorted([y1,y2])
+        columns = sorted([x1,x2])
         if x1 == x2:
             print(f'column {x1} to be marked in rows {y1, y2}')
-            rows = sorted([y1,y2])
-            for i in range(rows[0],rows[1]):
-                print(i)
+            for i in range(rows[0],rows[1] + 1):
                 new_matrix[i][x1] += 1
-                print(new_matrix)
+        elif y1 == y2:
+            print(f'row {y1} to be marked in columns {x1, x2}')
+            for i in range(columns[0],columns[1] + 1):
+                new_matrix[y1][i] += 1
+        else:
+            print(f'rows {y1, y2} to be marked in columns {x1, x2}')
+            x_counter = [i for i in range(columns[0],columns[1] + 1)]
+            y_counter = [i for i in range(rows[0],rows[1] + 1)]
+            if x1 < x2: x_counter.reverse()
+            if y1 < y2: y_counter.reverse()
+            for i in y_counter:
+                j = x_counter.pop(0)
+                new_matrix[i][j] += 1
+    return new_matrix
+
 
 vents = read_data(file_path)
 matrix = build_matrix(vents)
-mark_matrix(matrix, vents)
+new_matrix = mark_matrix(matrix, vents)
+# [print(i) for i in new_matrix]
+result = [[item >= 2 for item in row].count(True) for row in new_matrix]
+print(f'At least 2 line overlap detected {sum(result)} times')
