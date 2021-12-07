7e6422dc8e5f8d49a42aee1b0c663b77651058bb
konscodes <katashi.rain@gmail.com>
2021-12-07 13:40:38 +0900

    day6 part2 done

--- advent2021/day6.py
+++ advent2021/day6.py
@@ -1,5 +1,7 @@
 # Part1 
 # Part2 Performance decorator
+# Part2 speed issues with list.append() 
+# resolved by droping append in favor of different approach counting sum of increments for each newly created number.
 from time import time
 from pathlib import Path
 
@@ -23,8 +25,9 @@ def read_data(file_path):
         data = [int(i) for i in data]
     return data
 
+
 @performance
-def main_loop(data):
+def slow_loop(data):
     counter = data.copy()
     days = 60
     for day in range(days):
@@ -41,6 +44,49 @@ def main_loop(data):
     return counter
 
 
+@performance
+def faster_loop(data):
+    fish = data.copy()
+    days = 60
+    for day in range(days):
+        new_fish = []
+        for i, value in enumerate(fish):
+            if value == 0:
+                fish[i] = 6
+                new_fish.append(8)
+            elif 7 <= value <= 8:
+                fish[i] -= 1
+            else: 
+                fish[i] -= 1
+            #print(day, i, state, counter)
+        fish.extend(new_fish)
+    return fish
+
+
+@performance
+# Solution from
+# https://github.com/wmporter/aoc2021/blob/main/day/6/part2.py
+def the_fastest(data):
+    fish = data.copy()
+    days = 256
+    fish_added = [0] * days
+    # Instead of manipulating list of fish, we only increment all newly created fish
+    # Fish created from initial list
+    for f in fish:
+        fish_added[f] += 1
+        for i in range(f+7, days, 7):
+            fish_added[i] += 1
+    # Fish created from newly born fish thus + 2 days with step of 7 days
+    for day in range(days):
+        for d in range(day+9, days, 7):
+            fish_added[d] += fish_added[day]
+    return fish_added
+
+
 data = read_data(file_path)
-result = main_loop(data)
-print(f'Total {len(result)} fish')
+result1 = slow_loop(data)
+result2 = faster_loop(data)
+result3 = the_fastest(data)
+print(f'Total {len(result1)} fish')
+print(f'Total {len(result2)} fish')
+print(f'Total {sum(result3) + len(data)} fish')
